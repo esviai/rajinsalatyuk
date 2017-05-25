9b12c46d342b307e38bfc3270be6a74484742fcc
hakiemaul <noersedya@gmail.com>
2017-05-25 14:53:12 +0700

    fully functioning reply feature

--- controllers/searchTweet.js
+++ controllers/searchTweet.js
@@ -1,8 +1,10 @@
-const url =  `http://muslimsalat.com/`
-const end = `/daily.json?key=654f798a989f8b5cffccd98ba5b0daa6`
+const url =  `http://muslimsalat.com/`;
+const end = `/daily.json?key=654f798a989f8b5cffccd98ba5b0daa6`;
+const util = require('../helpers/util');
 require('dotenv').config();
-var Twit = require('twit')
-var getJSON = require('get-json')
+var Twit = require('twit');
+var getJSON = require('get-json');
+var convertTime = require('convert-time');
 
 var T = new Twit({
   consumer_key:process.env.CONSUMER_KEY,
@@ -12,10 +14,10 @@ var T = new Twit({
 })
 
 var latestTweet = function() {
-  T.get('statuses/user_timeline', { count: 1 })
-  .then(result => {
-    console.log('Searching for tweet')
-    let latest_tweet_id = result.data[0].id
+  T.get('statuses/user_timeline', { count: 1 }, function(err, data) {
+    if(err) console.log(err)
+    let latest_tweet_id = data[0].id;
+    console.log('Searching tweet')
     searchTweet(latest_tweet_id)
   })
 }
@@ -32,8 +34,10 @@ var searchTweet = function(since) {
       let context = data.text;
       context = context.split(',')
       let completeURL = `${url}${context[0]}${end}`
-      let request = context[1]
-
+      context[1] = context[1].trim();
+      let request = context[1].split(' ');
+      let time = util.requestGen(request[0])
+      if(time) status.specific = time;
       getJSON(completeURL, function(err, response) {
         status.time = response.items;
         status.place = response.query;
@@ -41,24 +45,46 @@ var searchTweet = function(since) {
           defaultTweet(status)
         } else {
           status.time = status.time[0];
-          replyTweet(status)
+          if(status.specific == undefined) {
+            replyAll(status)
+          } else {
+            replyOne(status)
+          }
         }
       })
     })
+    if(!result) console.log('No #rajinsalatyuk tweet detected')
+  })
+  .catch(err => {
+    console.log(err)
   })
 }
 
-var replyTweet = function(content) {
-  let hasil = `subuh ${content.time.fajr} zuhur ${content.time.dhuhr} ashar ${content.time.asr} maghrib ${content.time.maghrib} isha ${content.time.isha}`
-  T.post('statuses/update', { status: `@${content.username} Waktu sholat di ${content.place}, ${hasil}!`, in_reply_to_status_id: content.status_id }, function(err, data) {
-    console.log('Reply data kepada user ' + content.username)
-    console.log(data)
+var replyAll = function(content) {
+  let waktu = {
+    subuh: convertTime(content.time.fajr),
+    zuhur: convertTime(content.time.dhuhr),
+    ashar: convertTime(content.time.asr),
+    maghrib: convertTime(content.time.maghrib),
+    isha: convertTime(content.time.isha)
+  }
+  let hasil = `subuh ${waktu.subuh} zuhur ${waktu.zuhur} ashar ${waktu.ashar} maghrib ${waktu.maghrib} isya ${waktu.isha}`
+  T.post('statuses/update', { status: `@${content.username} Waktu sholat di ${content.place} adalah ${hasil} (waktu setempat)`, in_reply_to_status_id: content.status_id }, function(err, data) {
+    console.log('Reply all data to user ' + content.username)
+  })
+}
+
+var replyOne = function(content) {
+  let request = content.specific[1];
+  let hasil = convertTime(content.time[request]);
+  T.post('statuses/update', { status: `@${content.username} Waktu sholat ${content.specific[0]} di ${content.place} adalah jam ${hasil} waktu setempat. Yuk shalat!`, in_reply_to_status_id: content.status_id }, function(err, data) {
+    console.log('Reply specific data to user ' + content.username)
   })
 }
 
 var defaultTweet = function(content) {
   T.post('statuses/update', { status: `@${content.username} Untuk mengetahui waktu sholat di tempatmu, tweet nama lokasi dengan #rajinsalatyuk!`, in_reply_to_status_id: content.status_id }, function(err, data) {
-    console.log('Reply default kepada user ' + content.username)
+    console.log('Reply default to user ' + content.username)
   })
 }
 
@@ -66,4 +92,6 @@ var execute = function(req, res, send) {
   setInterval(latestTweet, 10000)
 }
 
-execute()
+module.exports = {
+  execute
+};
--- helpers/util.js
+++ helpers/util.js
@@ -1,9 +1,12 @@
 var requestGen = function(context) {
-  switch (context) {
-    case 'expression':
-
-      break;
-    default:
-
-  }
+  if(/buh\b/.test(context)) return [context, "fajr"];
+  if(/hur\b/.test(context)) return [context, "dhuhr"];
+  if(/ar\b/.test(context)) return [context, "asr"];
+  if(/rib\b/.test(context)) return [context, "maghrib"];
+  if(/a\b/.test(context)) return [context, "isha"];
+  return false;
 }
+
+module.exports = {
+  requestGen
+};
