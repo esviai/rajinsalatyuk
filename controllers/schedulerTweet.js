7379f0397b2927e394953a64e04d66875d551b50
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-05-25 12:15:03 +0700

    stuck at rendering salat time to proper array

--- controllers/schedulerTweet.js
+++ controllers/schedulerTweet.js
@@ -1,5 +1,7 @@
 const CronJob = require('cron').CronJob;
 const Twit = require('twit');
+const getJSON = require('get-json')
+
 
 var T = new Twit({
 consumer_key:process.env.CONSUMER_KEY, //consumer key
@@ -9,9 +11,8 @@ access_token_secret:process.env.ACCESS_TOKEN_SECRET, //test user secret
 timeout_ms:60*1000,  // optional HTTP request timeout to apply to all requests.
 })
 
-const time = function() {
-  let randomizer = Math.ceil(Math.random()*10000000000000)
-  T.post('statuses/update', { status: `${randomizer} Di twit tiap 1 detik #RajinSalatYuk`},
+const Tweeter = function() {
+  T.post('statuses/update', { status: ``},
     function(err, data) {
       if (err) {
         console.log(err);
@@ -21,11 +22,33 @@ const time = function() {
   })
 }
 
-const test = function(req,res) {
-  new CronJob('* * * * * *', time ,null ,true)
+
+
+const Scheduler = function(req,res) {
+  console.log("Masuk Scheduler");
+  var timePattern = `05 * * * * *`
+  new CronJob(timePattern, function() {
+    getJSON('http://muslimsalat.com/Jakarta/daily.json?key=654f798a989f8b5cffccd98ba5b0daa6', function(error, response){
+     if (response) {
+       var salatTime = []
+       let fajr = response.items[0].fajr.split("").
+       let dhuhr = response.items[0].dhuhr.split("")
+       let asr = response.items[0].asr.split("")
+       let maghrib = response.items[0].maghrib.split("")
+       let isha = response.items[0].isha.split("")
+       console.log(fajr,dhuhr,asr,maghrib,isha);
+      //  console.log(salatTime.push().push().push().push().push());
+     } else {
+       console.log(error)
+     }
+    })
+    console.log("Jalan");
+  } ,
+  function() {
+  } ,true)
   res.send("Bismillah")
 }
 
 module.exports = {
-  test
+  Scheduler
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const schedulerController = require('../controllers/schedulerTweet')
 
 //Test cron
-router.get('/',schedulerController.test)
+router.get('/',schedulerController.Scheduler)
 
 module.exports = router;
