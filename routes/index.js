980bce1d1b50faa1abac630a35fccd9ccaf7dca5
hakiemaul <noersedya@gmail.com>
2017-05-26 10:12:47 +0700

    auth done

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,18 @@ const router = express.Router();
 const schedulerController = require('../controllers/schedulerTweet');
 const searchTweet = require('../controllers/searchTweet');
 const lebaranTweet = require('../controllers/lebaranTweet');
+const auth = require('../controllers/auth');
 
 // Test cron
-router.get('/',schedulerController.allCity);
+router.get('/', schedulerController.allCity);
 
 // Search and reply tweet
 router.get('/reply', searchTweet.execute);
 
 // Lebaran tweet
 router.get('/sudahlebaran', lebaranTweet.create);
+
+// Credentials
+router.post('/signin', auth.adminLogin);
+
 module.exports = router;
